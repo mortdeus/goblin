9689e1f6d479948b75258ca55932ff9d7ccd7a48
Kutani <kutani@projectkutani.com>
2013-01-27 18:58:25 -0600

    strings(1) implemented

--- strings/main.go
+++ strings/main.go
@@ -1,1 +1,92 @@
 package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
+	"unicode"
+)
+
+const BUFSIZE int = 70
+
+var (
+	minspan = flag.Int("m", 6, "Defines the minimum span size for a series "+
+		"of runes to be considered a string.")
+	offsets = flag.Bool("o", false, "Print the file offset for the beginning "+
+		"of strings.")
+)
+
+func usage() {
+	fmt.Fprintf(os.Stderr, "Usage: strings [-o] [-m min] [file ...]\n")
+	flag.PrintDefaults()
+	os.Exit(2)
+}
+
+func error(s string) {
+	fmt.Fprint(os.Stderr, s, "\n")
+	os.Exit(1)
+}
+
+func prntstr(s string, start uint64) {
+	if( *offsets ) {
+		fmt.Printf("%8d: %s\n", start, s)
+	} else {
+		fmt.Printf("%s\n", s)
+	}
+}
+
+func stringit(f *os.File) {
+	buf := make([]rune, BUFSIZE)
+	var start uint64 = 0
+
+	b := bufio.NewReader(f)
+
+	for {
+		c, size, err := b.ReadRune()
+		if err != nil {
+			break
+		}
+
+		start += uint64(size)
+
+		if unicode.IsGraphic(c) {
+			buf = append(buf, c)
+			if len(buf) == BUFSIZE {
+				prntstr(string(buf)+" ...", start)
+				buf = buf[0:0]
+			}
+		} else {
+			if len(buf) >= *minspan {
+				prntstr(string(buf), start)
+				buf = buf[0:0]
+			}
+		}
+	}
+
+	if len(buf) >= *minspan {
+		prntstr(string(buf), start)
+	}
+}
+
+func main() {
+	flag.Usage = usage
+	flag.Parse()
+	args := flag.Args()
+
+	if len(args) == 0 {
+		stringit(os.Stdin)
+	} else {
+		for _, path := range args {
+			f, err := os.Open(path)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "open %s: %s", path, err)
+				continue
+			}
+			stringit(f)
+			f.Close()
+		}
+	}
+
+	os.Exit(0)
+}
