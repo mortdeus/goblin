bc9923f1e76dbf97da73ff2ce349ed50b26c3238
mortdeus <mortdeus@gocos2d.org>
2013-01-28 19:49:15 -0600

    cleaned up code and added "..." when len(string) > BUFSIZE

--- strings/main.go
+++ strings/main.go
@@ -11,9 +11,9 @@ import (
 const BUFSIZE int = 70
 
 var (
-	minspan = flag.Int("m", 6, "Defines the minimum span size for a series "+
+	minSpan = flag.Int("m", 6, "Defines the minimum span size for a series "+
 		"of runes to be considered a string.")
-	offsets = flag.Bool("o", false, "Print the file offsets for the "+
+	verbose = flag.Bool("v", false, "Print the file offsets for the "+
 		"beginning of strings.")
 )
 
@@ -28,50 +28,48 @@ func error(s string) {
 	os.Exit(1)
 }
 
-func prntstr(s string, start int64) {
-	if *offsets {
-		fmt.Printf("%8d: %s\n", start, s)
-	} else {
-		fmt.Printf("%s\n", s)
-	}
-}
-
-func stringit(f *os.File) {
+func makeString(f *os.File) {
 	buf := make([]rune, BUFSIZE)
-	var start, posn int64
-
 	b := bufio.NewReader(f)
 
-	posn = 0
-	start = 0
+	var position, start int64
+	printString := func(buf []rune, start int64) {
+		if *verbose {
+			fmt.Printf("%8d: %s\n", start, string(buf))
+		} else {
+			fmt.Printf("%s\n", string(buf))
+		}
+	}
+
 	for {
 		c, size, err := b.ReadRune()
 		if err != nil {
 			break
 		}
-		posn += int64(size)
+		position += int64(size)
 
 		if unicode.IsGraphic(c) {
 			if start == 0 {
-				start = posn
+				start = position
 			}
 			buf = append(buf, c)
 			if len(buf) == BUFSIZE {
-				prntstr(string(buf)+" ...", start)
+				buf = append(buf, []rune("...")...)
+				printString(buf, start)
 				buf = buf[0:0]
 				start = 0
 			}
 		} else {
-			if len(buf) >= *minspan {
-				prntstr(string(buf), start)
+			if len(buf) >= *minSpan {
+				printString(buf, start)
 				buf = buf[0:0]
 				start = 0
 			}
 		}
 	}
 
-	if len(buf) >= *minspan {
-		prntstr(string(buf), start)
+	if len(buf) >= *minSpan {
+		printString(buf, start)
 	}
 }
 
@@ -81,7 +79,7 @@ func main() {
 	args := flag.Args()
 
 	if len(args) == 0 {
-		stringit(os.Stdin)
+		makeString(os.Stdin)
 	} else {
 		for _, path := range args {
 			f, err := os.Open(path)
@@ -89,7 +87,7 @@ func main() {
 				fmt.Fprintf(os.Stderr, "open %s: %s", path, err)
 				continue
 			}
-			stringit(f)
+			makeString(f)
 			f.Close()
 		}
 	}
