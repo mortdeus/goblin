8ecefbef83fe800fa1caa166eb37f44d362201d4
mortdeus <mortdeus@gocos2d.org>
2013-01-29 00:26:51 -0600

    fixed usage doc

--- strings/main.go
+++ strings/main.go
@@ -18,7 +18,7 @@ var (
 )
 
 func usage() {
-	fmt.Fprintf(os.Stderr, "Usage: strings [-o] [-m min] [file ...]\n")
+	fmt.Fprintf(os.Stderr, "Usage: strings [-v] [-m min] [file ...]\n")
 	flag.PrintDefaults()
 	os.Exit(2)
 }
@@ -33,14 +33,14 @@ func makeString(f *os.File) {
 	b := bufio.NewReader(f)
 
 	var position, start int64
+	
 	printString := func(buf []rune, start int64) {
 		if *verbose {
 			fmt.Printf("%8d: %s\n", start, string(buf))
 		} else {
 			fmt.Printf("%s\n", string(buf))
-		}
 	}
-
+	
 	for {
 		c, size, err := b.ReadRune()
 		if err != nil {
